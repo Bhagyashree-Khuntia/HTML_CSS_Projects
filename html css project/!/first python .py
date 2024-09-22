de9e93be9aedbc7572adf2464780d21b11bcae86
Bhagyashree-Khuntia <bhagyashreekhuntia51@gmail.com>
2024-09-22 12:50:29 +0530

    Add files via upload

---
+++ html css project/!/first python .py
@@ -0,0 +1,3 @@
+name = ("suraj")
+desg = ("student")
+print("hello" + name + "your designation is" + desg)
---
+++ html css project/!/suraj.py
@@ -0,0 +1,5 @@
+a = int(input("Enter first number :56"))
+b = int(input("Enter second number :89"))
+c = int(input("Enter third number :94"))
+max_number = (a, b, c,)
+print (max_number)
